a41bec772497ade281704e63a054b3a4d5c37786
eduarladds <79010897+MyBusisinessPOS@users.noreply.github.com>
2021-06-17 09:08:28 -0500

    Credentials

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
   define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'eduarladds_dev' );             // Set database user
+  define( 'DB_USER', 'eduarlad_dev' );             // Set database user
   define( 'DB_PASS', ',.-3A601b14a0-.,' );             // Set database password
-  define( 'DB_NAME', 'eduarladds_plazamary' );        // Set database name
+  define( 'DB_NAME', 'eduarlad_plazamary' );        // Set database name
 
