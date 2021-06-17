c78a94772aa7f1410f49f71720bde24878486474
eduarladds <79010897+MyBusisinessPOS@users.noreply.github.com>
2021-06-17 09:04:47 -0500

    Config DB

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
   define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'soluc140_dev' );             // Set database user
+  define( 'DB_USER', 'eduarladds_dev' );             // Set database user
   define( 'DB_PASS', ',.-3A601b14a0-.,' );             // Set database password
-  define( 'DB_NAME', 'soluc140_apimary' );        // Set database name
+  define( 'DB_NAME', 'eduarladds_plazamary' );        // Set database name
 
